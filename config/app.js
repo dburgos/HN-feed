d63ef1a52e9799538a41c04bfbe7cb19de72cb8b
dburgos <david.burgos.risco@gmail.com>
2015-05-28 09:10:35 +0200

    Add config files

---
+++ config/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.app = {
+
+  settings: {
+    url: 'http://localhost',
+    port: 3000,
+    db: 'mongodb://localhost:27017/hn-feed',
+  },
+
+  feed: {
+    query: 'nodejs'
+  }
+
+};
---
+++ config/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var config 				= {};
+
+config.app     		= require('./app');
+
+module.exports 		= config;
