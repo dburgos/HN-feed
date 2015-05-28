eb43129ff2ce2ee10a799dcad0cbbc7d7bdad872
dburgos <david.burgos.risco@gmail.com>
2015-05-28 10:15:06 +0200

    Minor fixes in order to start up the app

--- app.js
+++ app.js
@@ -6,20 +6,18 @@ var express         = require('express');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var compress        = require('compression');
-var favicon         = require('serve-favicon');
 var multipart       = require('connect-multiparty');
 var app             = express();
 
 // Configuration
 app.enable('trust proxy');
-app.set('port', config.app.port);
+app.set('port', config.app.settings.port);
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 app.use(compress());
-app.use(favicon(__dirname + '/public/img/favicon.png'));
-app.use(bodyParser({ limit: '128kb'}));
+app.use(bodyParser.json({ limit: '128kb'}));
 app.use(multipart());
-app.use(express.static(__dirname+'/public'), { maxAge: 86400000 });
+app.use(express.static('public'));
 app.use(methodOverride());
 
 // Routes
