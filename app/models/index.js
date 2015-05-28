84006c57efb1dfbb845ea662c4466f9d4005b461
dburgos <david.burgos.risco@gmail.com>
2015-05-28 09:51:19 +0200

    Add models

---
+++ app/models/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var model = {};
+
+model.Post   = require('./post');
+
+module.exports = model;
---
+++ app/models/post.js
@@ -0,0 +1,20 @@
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+var safeOpts    = {
+  j:        1,
+  w:        "majority",
+  wtimeout: 10000
+};
+
+var postSchema  = new Schema({
+  saved_at:     { type: Date, default: Date.now, required: true},
+  created_at:   { type: Date, required: true},
+  created_at_i: { type: Number, required: true},
+  url:          String,
+  title:        String,
+  story_title:  String,
+  author:       String,
+  num_comments: Number
+}, { safe: safeOpts });
+
+module.exports    = postSchema;
